0946e9a745e301d941e3ae8db1233a5aefdd4f17
daveed <david.alphen@gmail.com>
2012-11-21 18:26:57 +0800

    adding a helper method to add the chat div in the page

--- app/helpers/whisper/application_helper.rb
+++ app/helpers/whisper/application_helper.rb
@@ -1,4 +1,11 @@
 module Whisper
   module ApplicationHelper
+
+    def whisper_for(user, opts = {})
+      username = user.username
+      userid   = user.id
+      render :partial => "whisper/chat/popin", :locals => {:userid => userid, :username => username, :opts => opts}
+    end
+
   end
 end
