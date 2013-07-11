60ae66b68bc943744955d1ca5c3961db98976207
daveed <david.alphen@gmail.com>
2013-07-11 17:01:48 +0800

    faye integration wip

--- app/controllers/whisper/chats_controller.rb
+++ app/controllers/whisper/chats_controller.rb
@@ -17,51 +17,47 @@ module Whisper
         user_id = whisper_user.id
         @chat   = Chat.create_row(user_id, from, to, message, sent_at)
 
-
         PrivatePub.publish_to("/chats/new", chat: @chat)
 
-        respond_to do |format|
-          format.js
-          # format.json { render :json => @chat }
-        end
+        # respond_to do |format|
+        #   # format.json { render :json => @chat }
 
+        #   # whisper
+        #   # ex: /carlb so you spin this coming friday?
+        #   if message.start_with?("/")
+        #     to = message.split(' ')[0][1..-1].downcase
+        #     to_user = User.where("lower(username) = ?", to).first
+        #     message = message.split(' ')[1..-1].join(' ')
 
-        # # whisper
-        # # ex: /carlb so you spin this coming friday?
-        # if message.start_with?("/")
-        #   to = message.split(' ')[0][1..-1].downcase
-        #   to_user = User.where("lower(username) = ?", to).first
-        #   message = message.split(' ')[1..-1].join(' ')
+        #     if to_user.nil?
+        #       # user does not exist
+        #       PrivatePub.publish_to("/chats/new", chat: @chat)
+        #       # Pusher['private-user-channel-' + whisper_user.id.to_s].trigger('send-user-message', {:message => "#{to} does not exist", :from => from, :to => from})
+        #       format.json { head :unprocessable_entity }
+        #     elsif to_user
+        #       # whisper
+        #       PrivatePub.publish_to("/chats/new/#{to_user.id.to_s}", chat: @chat)
+        #       # Pusher['private-user-channel-' + to_user.id.to_s].trigger('send-user-message', {:message => message, :from => from, :to => to})
+        #       format.json { render json: @chat, status: :ok }
+        #     end
 
-        #   if to_user.nil?
-        #     # user does not exist
-        #     PrivatePub.publish_to("/chats/new", chat: @chat)
-        #     # Pusher['private-user-channel-' + whisper_user.id.to_s].trigger('send-user-message', {:message => "#{to} does not exist", :from => from, :to => from})
-        #     head :unprocessable_entity
-        #   elsif to_user
-        #     # whisper
-        #     PrivatePub.publish_to("/chats/new", chat: @chat)
-        #     # Pusher['private-user-channel-' + to_user.id.to_s].trigger('send-user-message', {:message => message, :from => from, :to => to})
-        #     head :ok
-        #   end
+        #   # team
+        #   elsif params[:from].eql?("team")
+        #     if !whisper_user.team.nil?
+        #       PrivatePub.publish_to("/chats/new", chat: @chat)
+        #       # Pusher['private-team-channel-' + whisper_user.team.id.to_s].trigger('send-team-message', {:message => message, :from => from, :to => whisper_user.team.name})
+        #       format.json { head :ok }
+        #     else
+        #       PrivatePub.publish_to("/chats/new", chat: @chat)
+        #       # Pusher['private-user-channel-' + whisper_user.id.to_s].trigger('send-user-message', {:message => "join a team first!", :from => from, :to => from})
+        #       format.json { head :unprocessable_entity }
+        #     end
 
-        # # team
-        # elsif params[:team].eql?("true")
-        #   if !whisper_user.team.nil?
-        #     PrivatePub.publish_to("/chats/new", chat: @chat)
-        #     # Pusher['private-team-channel-' + whisper_user.team.id.to_s].trigger('send-team-message', {:message => message, :from => from, :to => whisper_user.team.name})
-        #     head :ok
+        #   # broadcast
         #   else
         #     PrivatePub.publish_to("/chats/new", chat: @chat)
-        #     # Pusher['private-user-channel-' + whisper_user.id.to_s].trigger('send-user-message', {:message => "join a team first!", :from => from, :to => from})
-        #     head :unprocessable_entity
+        #     #format.json { render json: @chat, status: :ok }
         #   end
-
-        # # broadcast
-        # else
-        #   PrivatePub.publish_to("/chats/new", chat: @chat)
-        #   # Pusher['presence-channel'].trigger('send-message', {:message => message, :from => from})
-        #   head :ok
         # end
 
       end
--- app/helpers/whisper/application_helper.rb
+++ app/helpers/whisper/application_helper.rb
@@ -2,7 +2,7 @@ module Whisper
   module ApplicationHelper
 
     def whisper_for(user, opts = {})
-      render :partial => "whisper/chat/popin", :locals => {:current_user => user, :opts => opts}
+      render :partial => "whisper/chats/popin", :locals => {:current_user => user, :opts => opts}
     end
 
   end
